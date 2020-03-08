9e02a0a3d9f6c453b105f7c7b4062d8970a96078
kasper steenstrup <kasper.steenstrup@3shape.com>
2020-03-08 22:40:25 +0100

    init

---
+++ Model/NPC.cs
@@ -0,0 +1,15 @@
+﻿namespace ParanoiaRedEditionWasm.Model
+{
+    public class NPC
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Clearance { get; set; }
+        public int CloneNumber { get; set; }
+        public string Decription { get; set; }
+        public string Zone { get; set; }
+        public string Image { get; set; }
+        public int Moxie { get; set; }
+        public int TreasonStars { get; set; }
+    }
+}
